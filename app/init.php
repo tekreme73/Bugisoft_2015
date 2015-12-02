f828cca9e89ebd75c27435ab3492b4007f64aa61
remi.r <tekreme73@gmail.com>
2015-12-02 19:09:00 +0000

    Initial architecture creation :boom:

---
+++ app/init.php
@@ -0,0 +1,6 @@
+<?php
+
+// Composer autoloader
+require_once '../vendor/autoload.php';
+
+//TODO Initialize application
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../app/init.php';
+
+//TODO Launch application created by init.php file
+
+echo "Hello World!";
